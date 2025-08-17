6036c697a5f5e3670b7358bef2e71054631b90d3
Mohsen <sangsefidimohsen84@gmail.com>
2025-08-17 13:11:33 +0330

    Create budget api - GET POST PUT DELETE

--- budgets/serializers.py
+++ budgets/serializers.py
@@ -1,1 +1,20 @@
 from rest_framework import serializers
+from .models import BudgetModel
+
+
+class BudgetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BudgetModel
+        fields = '__all__'
+        read_only_fields = ['id', 'user']
+
+
+class UpdateBudgetSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(required=False)
+    total_amount = serializers.IntegerField(required=False)
+    start_date = serializers.DateField(required=False)
+    end_date = serializers.DateField(required=False)
+
+    class Meta:
+        fields = ['id', 'title', 'total_amount', 'start_date', 'end_date']
--- budgets/views.py
+++ budgets/views.py
@@ -1,3 +1,189 @@
-from django.shortcuts import render
+from django.db import IntegrityError
 
-# Create your views here.
+from utils.polls import authenticate_jwt_token
+from rest_framework.response import Response
+from rest_framework import views, status
+
+from budgets.serializers import BudgetSerializer, UpdateBudgetSerializer
+
+from .models import BudgetModel
+
+
+class BudgetView(views.APIView):
+    def get(self, request, *args, **kwargs):
+        # Validate user authentications
+        jwt_token = request.COOKIES.get('jwt_token')
+
+        result, response, user = authenticate_jwt_token(jwt_token)
+
+        if result:
+            return response
+
+        budgets = BudgetModel.objects.filter(user=user)
+
+        serializer = BudgetSerializer(budgets, many=True)
+
+        return Response(
+            {
+                'result': True,
+                'massage': 'successfully',
+                'data': serializer.data
+            }, status=status.HTTP_200_OK
+        )
+
+    def post(self, request, *args, **kwargs):
+        # Validate user authentications
+        jwt_token = request.COOKIES.get('jwt_token')
+
+        result, response, user = authenticate_jwt_token(jwt_token)
+
+        if result:
+            return response
+
+        # Validate data
+        serializer = BudgetSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(
+                {
+                    'result': False,
+                    'massage': 'Invalid data',
+                    'data': serializer.errors
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # Data
+        title = serializer.validated_data.get('title')
+        total_amount = serializer.validated_data.get('total_amount')
+        start_date = serializer.validated_data.get('start_date')
+        end_date = serializer.validated_data.get('end_date')
+
+        # Create Budget
+        try:
+            budget = BudgetModel.objects.create(
+                title=title,
+                total_amount=total_amount,
+                start_date=start_date,
+                end_date=end_date,
+                user_id=user.id
+            )
+        except IntegrityError:
+            return Response(
+                {
+                    'result': False,
+                    'massage': 'Invalid data',
+                    'data': []
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # Return response and data
+        return Response(
+            {
+                'result': True,
+                'massage': 'Created budget successfully',
+                'data': [BudgetSerializer(budget).data]
+            }, status=status.HTTP_201_CREATED
+        )
+
+    def put(self, request, *args, **kwargs):
+        # Validate user authentications
+        jwt_token = request.COOKIES.get('jwt_token')
+
+        result, response, user = authenticate_jwt_token(jwt_token)
+
+        if result:
+            return response
+
+        # Validate data
+        serializer = UpdateBudgetSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(
+                {
+                    'result': False,
+                    'massage': 'Invalid data',
+                    'data': serializer.errors
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # Data
+        budget_id = serializer.validated_data.get('id')
+        title = serializer.validated_data.get('title')
+        total_amount = serializer.validated_data.get('total_amount')
+        start_date = serializer.validated_data.get('start_date')
+        end_date = serializer.validated_data.get('end_date')
+
+        # Get Budget
+        try:
+            budget = BudgetModel.objects.get(id=budget_id, user_id=user.id)
+        except BudgetModel.DoesNotExist:
+            return Response(
+                {
+                    'result': False,
+                    'massage': 'Budget does not exist',
+                    'data': []
+                }
+            )
+
+        # Update budget
+        budget.title = title if title else budget.title
+        budget.total_amount = total_amount if total_amount else budget.total_amount
+        budget.start_date = start_date if start_date else budget.start_date
+        budget.end_date = end_date if end_date else budget.end_date
+
+        budget.save()  # Save
+
+        return Response(
+            {
+                'result': True,
+                'massage': 'Updated budget successfully',
+                'data': [BudgetSerializer(budget).data]
+            }, status=status.HTTP_200_OK
+        )
+
+    def delete(self, request, *args, **kwargs):
+        # Validate user authentications
+        jwt_token = request.COOKIES.get('jwt_token')
+
+        result, response, user = authenticate_jwt_token(jwt_token)
+
+        if result:
+            return response
+
+        # Validate data
+        serializer = UpdateBudgetSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(
+                {
+                    'result': False,
+                    'massage': 'Invalid data',
+                    'data': serializer.errors
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # Data
+        budget_id = serializer.validated_data.get('id')
+
+        # Get budget
+        try:
+            budget = BudgetModel.objects.get(id=budget_id, user_id=user.id)
+        except BudgetModel.DoesNotExist:
+            return Response(
+                {
+                    'result': False,
+                    'massage': 'Budget does not exist',
+                    'data': []
+                }
+            )
+
+        # Delete budget
+        budget.delete()
+
+        return Response(
+            {
+                'result': True,
+                'massage': 'Deleted budget successfully',
+                'data': []
+            }, status=status.HTTP_200_OK
+        )
